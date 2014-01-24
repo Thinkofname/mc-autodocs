d2986c97ac34ec3b0a19d814c5fca61a10f048a2
Thinkofdeath <thethinkofdeath@gmail.com>
2014-01-24 19:18:09 +0000

    14w04b

--- play/PacketClientbound10.java
+++ play/PacketClientbound10.java
@@ -2,40 +2,32 @@
 public class PacketClientbound10 extends gf {
 
    private int a;
-   private int b;
-   private int c;
-   private int d;
-   private int e;
-   private String f;
+   private Position b;
+   private de c;
+   private String d;
 
 
    public PacketClientbound10() {}
 
    public PacketClientbound10(sx var1) {
       this.a = var1.A();
-      this.b = var1.b;
-      this.c = var1.c;
-      this.d = var1.d;
-      this.e = var1.a.b();
-      this.f = var1.e.B;
+      this.b = var1.i();
+      this.c = var1.b;
+      this.d = var1.c.B;
    }
 
    public void read(PacketBuffer var1) {
       this.a = var1.readVarInt();
-      this.f = var1.readString(sy.A);
-      this.b = var1.readInt();
-      this.c = var1.readInt();
-      this.d = var1.readInt();
-      this.e = var1.readInt();
+      this.d = var1.readString(sy.A);
+      this.b = var1.readPosition();
+      this.c = de.b(var1.readUnsignedByte());
    }
 
    public void write(PacketBuffer var1) {
       var1.writeVarInt(this.a);
-      var1.writeString(this.f);
-      var1.writeInt(this.b);
-      var1.writeInt(this.c);
-      var1.writeInt(this.d);
-      var1.writeInt(this.e);
+      var1.writeString(this.d);
+      var1.writePosition(this.b);
+      var1.writeByte(this.c.b());
    }
 
    public void a(gi var1) {
@@ -43,6 +35,6 @@ public class PacketClientbound10 extends gf {
    }
 
    public String b() {
-      return String.format("id=%d, type=%s, x=%d, y=%d, z=%d", new Object[]{Integer.valueOf(this.a), this.f, Integer.valueOf(this.b), Integer.valueOf(this.c), Integer.valueOf(this.d)});
+      return String.format("id=%d, type=%s, x=%d, y=%d, z=%d, facing=%s", new Object[]{Integer.valueOf(this.a), this.d, Integer.valueOf(this.b.n()), Integer.valueOf(this.b.o()), Integer.valueOf(this.b.p()), this.c});
    }
 }
